848fde5a6ad471ff954d5ef3a963de085f9cc895
Matěj Marčišovský <matej@marcisovsky.cz>
2018-10-03 20:01:42 +0200

    temp

---
+++ _index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const as = require('./index')
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use('/image', as);
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
